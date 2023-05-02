e04a262c722cd9241292afa5308bb52dd41fe4ce
haoran <haoran.yi@solana.com>
2023-05-02 03:34:10 +0000

    cow

--- sdk/program/src/pubkey.rs
+++ sdk/program/src/pubkey.rs
@@ -919,4 +919,34 @@ mod tests {
         );
         assert!(pubkey_from_seed_by_marker(&PDA_MARKER[1..]).is_ok());
     }
+    #[test]
+    fn test_tt() {
+        use std::borrow::Cow;
+
+        fn abs_all(input: &mut Cow<[i32]>) {
+            for i in 0..input.len() {
+                let v = input[i];
+                if v < 0 {
+                    // Clones into a vector if not already owned.
+                    // slice. fn to_owned(&self) -> Vec<T, Global>
+                    input.to_mut()[i] = -v;
+                }
+            }
+        }
+
+        // No clone occurs because `input` doesn't need to be mutated.
+        let slice = [0, 1, 2];
+
+        let mut input = Cow::from(&slice[..]);
+        abs_all(&mut input);
+
+        // Clone occurs because `input` needs to be mutated.
+        let slice = [-1, 0, 1];
+        let mut input = Cow::from(&slice[..]);
+        abs_all(&mut input);
+
+        // No clone occurs because `input` is already owned.
+        let mut input = Cow::from(vec![-1, 0, 1]);
+        abs_all(&mut input);
+    }
 }
