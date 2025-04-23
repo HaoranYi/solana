deb0b40f35abeb163152b12c764c1031c750607f
HaoranYi <219428+HaoranYi@users.noreply.github.com>
2025-04-23 14:06:34 +0000

    set epoch threadpool to 4 threads

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -1645,6 +1645,7 @@ impl Bank {
         let epoch = self.epoch();
         let slot = self.slot();
         let (thread_pool, thread_pool_time_us) = measure_us!(ThreadPoolBuilder::new()
+            .num_threads(4)
             .thread_name(|i| format!("solBnkNewEpch{i:02}"))
             .build()
             .expect("new rayon threadpool"));
